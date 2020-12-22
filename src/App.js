322715b05eb608299c6169694b04fabbfe461bc3
Sonal <engineersonal@rediffmail.com>
2020-12-22 15:25:02 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import Header from "./Header";
+import "./App.css";
 
 function App() {
+  let subscribers = [
+    {
+      id: 1,
+      name: "Shilpa Bhat",
+      phone: "8888888888",
+    },
+    {
+      id: 2,
+      name: "Srishti Gupta",
+      phone: "9999999999",
+    },
+  ];
   return (
-    <div>
+    <div className="component-body-container">
       <Header />
-      <button>Add</button>
-      <div>
-        <span>Name</span> <br />
-        <span>Phone</span>
+      <button className="custom-btn add-btn">Add</button>
+      <div className="grid-container heading-container">
+        <span className="grid-item name-heading">Name</span>
+        <span className="grid-item phone-heading">Phone</span>
       </div>
+      {subscribers.map((sub) => {
+        return (
+          <div key={sub.id} className="grid-container">
+            <span className="grid-item">{sub.name}</span>
+            <span className="grid-item">{sub.phone}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
